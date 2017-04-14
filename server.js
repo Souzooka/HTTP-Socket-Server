c832c37f4f0c357a4e3a781e10daf0c74903db50
Dakota <bazookaharwell@gmail.com>
2017-04-13 17:04:17 -1000

    Refactored server.js to improve readability.

--- server.js
+++ server.js
@@ -11,74 +11,68 @@ const server = net.createServer( (c) => {
     const path = requestLine[1];
     const httpVersion = requestLine[2];
 
-    let file = null;
-    let headerAndBody = null;
+    let response = null;
 
-    switch(method) {
-      case 'GET':
-        switch(path) {
-          case '/':
-          case '/index':
-          case '/index.html':
-            file = fs.readFileSync('index.html', 'utf8');
-            headerAndBody = `HTTP/1.1 200 OK
+    if (method === "GET") {
+      response = getResource(path, httpVersion);
+      c.write(response);
+      c.end();
+    }
+  });
+});
+
+function getResource(path, httpVersion) {
+  let file = null;
+  let headerAndBody = null;
+  switch(path) {
+    case '/':
+    case '/index':
+    case '/index.html':
+      file = fs.readFileSync('index.html', 'utf8');
+      headerAndBody = `HTTP/1.1 200 OK
 Content-Type : text/html
 Content-Length: ${file.length}
 
 ${file}`;
-            c.write(headerAndBody);
-            c.end();
-            break;
-          case '/hydrogen':
-          case '/hydrogen.html':
-            file = fs.readFileSync('hydrogen.html', 'utf8');
-            headerAndBody = `HTTP/1.1 200 OK
+      return headerAndBody;
+    case '/hydrogen':
+    case '/hydrogen.html':
+      file = fs.readFileSync('hydrogen.html', 'utf8');
+      headerAndBody = `HTTP/1.1 200 OK
 Content-Type : text/html
 Content-Length: ${file.length}
 
 ${file}`;
-            c.write(headerAndBody);
-            c.end();
-            break;
-          case '/helium':
-          case '/helium.html':
-            file = fs.readFileSync('helium.html', 'utf8');
-            headerAndBody = `HTTP/1.1 200 OK
+      return headerAndBody;
+    case '/helium':
+    case '/helium.html':
+      file = fs.readFileSync('helium.html', 'utf8');
+      headerAndBody = `HTTP/1.1 200 OK
 Content-Type : text/html
 Content-Length: ${file.length}
 
 ${file}`;
-            c.write(headerAndBody);
-            c.end();
-            break;
-          case '/css/styles':
-          case '/css/styles.css':
-            file = fs.readFileSync('css/styles.css', 'utf8');
-            headerAndBody = `HTTP/1.1 200 OK
+      return headerAndBody;
+    case '/css/styles':
+    case '/css/styles.css':
+      file = fs.readFileSync('css/styles.css', 'utf8');
+      headerAndBody = `HTTP/1.1 200 OK
 Content-Type : text/css
 Content-Length: ${file.length}
 
 ${file}`;
-            c.write(headerAndBody);
-            c.end();
-            break;
-          case '/404':
-          case '/404.html':
-          default:
-            file = fs.readFileSync('404.html', 'utf8');
-            headerAndBody = `HTTP/1.1 404 NOT FOUND
+      return headerAndBody;
+    case '/404':
+    case '/404.html':
+    default:
+      file = fs.readFileSync('404.html', 'utf8');
+      headerAndBody = `HTTP/1.1 404 NOT FOUND
 Content-Type : text/html
 Content-Length: ${file.length}
 
 ${file}`;
-            c.write(headerAndBody);
-            c.end();
-        }
-        break;
-      default:
-        break;
-    }
-  });
-});
+      return headerAndBody;
+  }
+}
 
 server.listen(8080);
