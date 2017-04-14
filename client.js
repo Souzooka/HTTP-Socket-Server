9be19870f38f53f7bd1a8b673ea8e6924b3e7d33
Dakota <bazookaharwell@gmail.com>
2017-04-13 16:58:12 -1000

    Write a client.js which can send requests to server

--- client.js
+++ client.js
@@ -1,15 +1,17 @@
 const net = require('net');
 const request = [process.argv[2], process.argv[3], process.argv[4]];
-console.log(request);
 
 if (!request[0] || !request[1] || !request[2]) {
   throw new Error('Usage is "client.js <METHOD> <PATH> <HTTPVERSION>"');
 }
 
-if (!request[0].toLowerCase() !== 'get') {
-  throw new Error('Only GET requests are implemented.');
-}
+const client = net.connect({port:8080}, () => {
 
-net.connect({port:8080}, () => {
+  const header = `${request[0]} ${request[1]} ${request[2]}`;
+  client.write(header);
 
+  client.on('data', (data) => {
+    const response = data.toString().split('\n\n');
+    process.stdout.write(response[1].toString());
+  });
 });
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const net = require('net');
 const fs = require('fs');
 
-const server= net.createServer( (c) => {
+const server = net.createServer( (c) => {
 
   c.on('data', (data) => {
     const header = data.toString().split('\r\n');
@@ -14,63 +14,69 @@ const server= net.createServer( (c) => {
     let file = null;
     let headerAndBody = null;
 
-    switch(path) {
-      case '/':
-      case '/index':
-      case '/index.html':
-        file = fs.readFileSync('index.html', 'utf8');
-        headerAndBody = `HTTP/1.1 200 OK
+    switch(method) {
+      case 'GET':
+        switch(path) {
+          case '/':
+          case '/index':
+          case '/index.html':
+            file = fs.readFileSync('index.html', 'utf8');
+            headerAndBody = `HTTP/1.1 200 OK
 Content-Type : text/html
 Content-Length: ${file.length}
 
 ${file}`;
-        c.write(headerAndBody);
-        c.end();
-        break;
-      case '/hydrogen':
-      case '/hydrogen.html':
-        file = fs.readFileSync('hydrogen.html', 'utf8');
-        headerAndBody = `HTTP/1.1 200 OK
+            c.write(headerAndBody);
+            c.end();
+            break;
+          case '/hydrogen':
+          case '/hydrogen.html':
+            file = fs.readFileSync('hydrogen.html', 'utf8');
+            headerAndBody = `HTTP/1.1 200 OK
 Content-Type : text/html
 Content-Length: ${file.length}
 
 ${file}`;
-        c.write(headerAndBody);
-        c.end();
-        break;
-      case '/helium':
-      case '/helium.html':
-        file = fs.readFileSync('helium.html', 'utf8');
-        headerAndBody = `HTTP/1.1 200 OK
+            c.write(headerAndBody);
+            c.end();
+            break;
+          case '/helium':
+          case '/helium.html':
+            file = fs.readFileSync('helium.html', 'utf8');
+            headerAndBody = `HTTP/1.1 200 OK
 Content-Type : text/html
 Content-Length: ${file.length}
 
 ${file}`;
-        c.write(headerAndBody);
-        c.end();
-        break;
-      case '/css/styles':
-      case '/css/styles.css':
-        file = fs.readFileSync('css/styles.css', 'utf8');
-        headerAndBody = `HTTP/1.1 200 OK
+            c.write(headerAndBody);
+            c.end();
+            break;
+          case '/css/styles':
+          case '/css/styles.css':
+            file = fs.readFileSync('css/styles.css', 'utf8');
+            headerAndBody = `HTTP/1.1 200 OK
 Content-Type : text/css
 Content-Length: ${file.length}
 
 ${file}`;
-        c.write(headerAndBody);
-        c.end();
-        break;
-      case '/404':
-      case '/404.html':
-      default:
-        file = fs.readFileSync('404.html', 'utf8');
-        headerAndBody = `HTTP/1.1 404 NOT FOUND
+            c.write(headerAndBody);
+            c.end();
+            break;
+          case '/404':
+          case '/404.html':
+          default:
+            file = fs.readFileSync('404.html', 'utf8');
+            headerAndBody = `HTTP/1.1 404 NOT FOUND
 Content-Type : text/html
 Content-Length: ${file.length}
 
 ${file}`;
-        c.write(headerAndBody);
-        c.end();
+            c.write(headerAndBody);
+            c.end();
+        }
+        break;
+      default:
+        break;
     }
   });
 });
