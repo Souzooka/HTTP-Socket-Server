9a397a0fdd44b4b4466337091b7fcf20993946d6
Dakota <bazookaharwell@gmail.com>
2017-04-13 16:19:10 -1000

    Added all other resources to the server.

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const server= net.createServer( (c) => {
 
     switch(path) {
       case '/':
+      case '/index':
       case '/index.html':
         file = fs.readFileSync('index.html', 'utf8');
         headerAndBody = `HTTP/1.1 200 OK
@@ -26,6 +27,41 @@ ${file}`;
         c.write(headerAndBody);
         c.end();
         break;
+      case '/hydrogen':
+      case '/hydrogen.html':
+        file = fs.readFileSync('hydrogen.html', 'utf8');
+        headerAndBody = `HTTP/1.1 200 OK
+Content-Type : text/html
+Content-Length: ${file.length}
+
+${file}`;
+        c.write(headerAndBody);
+        c.end();
+        break;
+      case '/helium':
+      case '/helium.html':
+        file = fs.readFileSync('helium.html', 'utf8');
+        headerAndBody = `HTTP/1.1 200 OK
+Content-Type : text/html
+Content-Length: ${file.length}
+
+${file}`;
+        c.write(headerAndBody);
+        c.end();
+        break;
+      case '/css/styles':
+      case '/css/styles.css':
+        file = fs.readFileSync('css/styles.css', 'utf8');
+        headerAndBody = `HTTP/1.1 200 OK
+Content-Type : text/css
+Content-Length: ${file.length}
+
+${file}`;
+        c.write(headerAndBody);
+        c.end();
+        break;
+      case '/404':
+      case '/404.html':
       default:
         file = fs.readFileSync('404.html', 'utf8');
         headerAndBody = `HTTP/1.1 404 NOT FOUND
