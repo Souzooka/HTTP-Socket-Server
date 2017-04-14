ffb032562ffb3f878f86c25d4ef552b7818b31d2
Dakota <bazookaharwell@gmail.com>
2017-04-13 16:31:05 -1000

    Implement client argument validation

--- client.js
+++ client.js
@@ -1,4 +1,14 @@
 const net = require('net');
+const request = [process.argv[2], process.argv[3], process.argv[4]];
+console.log(request);
+
+if (!request[0] || !request[1] || !request[2]) {
+  throw new Error('Usage is "client.js <METHOD> <PATH> <HTTPVERSION>"');
+}
+
+if (!request[0].toLowerCase() !== 'get') {
+  throw new Error('Only GET requests are implemented.');
+}
 
 net.connect({port:8080}, () => {
 
